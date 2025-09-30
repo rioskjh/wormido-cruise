0a027296c93ef72717f8c3389df5348301c96c7c
rioskjh <rioskjh@gmail.com>
2025-09-30 14:25:48 +0900

    공지사항 게시글 하단 border 추가 - #ddd 색상 1px

--- components/MobileAnnouncementSection.tsx
+++ components/MobileAnnouncementSection.tsx
@@ -106,8 +106,7 @@ export default function MobileAnnouncementSection() {
       </div>
       <div className="content-stretch flex flex-col gap-[10px] items-start relative shrink-0 w-full">
         {/* 공지사항 목록 - 모바일 버전 */}
-        <div className="box-border content-stretch flex flex-col gap-[2px] items-start pb-[10px] pt-0 px-0 relative shrink-0 w-full">
-          <div aria-hidden="true" className="absolute border-[#dddddd] border-[0px_0px_1px] border-solid inset-0 pointer-events-none" />
+        <div className="box-border content-stretch flex flex-col gap-[2px] items-start pb-[10px] pt-0 px-0 relative shrink-0 w-full border-b border-[#dddddd]">
           <div className="font-['Pretendard:Medium',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#222222] text-[13px] text-nowrap">
             <p className="leading-[20px] whitespace-pre">2025 여름 불꽃크루즈!! 7월19일부터 매주 토요일</p>
           </div>
@@ -115,8 +114,7 @@ export default function MobileAnnouncementSection() {
             <p className="leading-[18px] whitespace-pre">2025-06-09</p>
           </div>
         </div>
-        <div className="box-border content-stretch flex flex-col gap-[2px] items-start pb-[10px] pt-0 px-0 relative shrink-0 w-full">
-          <div aria-hidden="true" className="absolute border-[#dddddd] border-[0px_0px_1px] border-solid inset-0 pointer-events-none" />
+        <div className="box-border content-stretch flex flex-col gap-[2px] items-start pb-[10px] pt-0 px-0 relative shrink-0 w-full border-b border-[#dddddd]">
           <div className="font-['Pretendard:Medium',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#222222] text-[13px] text-nowrap">
             <p className="leading-[20px] whitespace-pre">2025년 정기검사 휴항안내</p>
           </div>
